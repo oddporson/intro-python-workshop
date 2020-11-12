9e41e4b79fb332a1a7a08424d8dd1e7c88bcb7b7
Porson <porsonlee@gmail.com>
2020-11-12 11:55:10 -0800

    improved it by adding input for user to enter the number and store it in subtotal variable

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 # print(msg)
 
 
-subtotal = 20.20
+subtotal = float(input("What is the subtotal? "))
 tax_rate = 0.14
 
 tax = subtotal * tax_rate
-total = subtotal + tax  # 23.028
-total = round(total, 2)  # 23.03
+total = subtotal + tax
+total = round(total, 2)
 print(total)
