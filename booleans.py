c3dfb7ca65b360c072d60eef1a8e9b58f248d4db
Porson <porsonlee@gmail.com>
2020-11-12 12:03:56 -0800

    initial commit

---
+++ booleans.py
@@ -0,0 +1,48 @@
+a = True
+b = False
+
+# --------------
+# Comparison Operators
+a = 14
+b = 5
+
+# equals
+a == b
+
+# doesn't equal
+a != b
+
+# greater than
+a > b
+
+# greater than or equal to
+a >= b
+
+# less than
+a < b
+
+# less than or equal to
+a <= b
+
+# equality with strings
+a = '1 2 3'
+b = 'one two 3'
+b == b
+a != b
+
+# --------------
+# logic operators (compound boolean expressions)
+
+# `and`
+temperature = 28
+sunny = True
+temperature > 25 and sunny
+sunny = False
+temperature > 25 and sunny
+
+# `or`
+has_coffee = True
+has_beer = True
+has_coffee or has_beer
+has_coffee = False
+has_coffee or has_beer
