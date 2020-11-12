0513a56bb8d27a135306c16c3f5699db60f9e418
Porson <porsonlee@gmail.com>
2020-11-12 11:41:13 -0800

    rounding numbers

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ subtotal = 20.20
 tax_rate = 0.14
 
 tax = subtotal * tax_rate
-total = subtotal + tax
+total = subtotal + tax  # 23.028
+total = round(total, 2)  # 23.03
 print(total)
---
+++ numbers.py
@@ -0,0 +1,23 @@
+# integers and floats
+a = -2
+b = 12.5
+
+# arithmetic
+c = a + 1    # Arithmetic with literals and variables
+c = a + b    # Arithmetic with just variables
+c = c + a    # c becomes its old value + a
+c = a - b    # Subtraction
+c = a * b    # Multiplication
+c = b / a    # Division
+c = (a + b) * 2 - a    # Compound calculations
+print(c)
+
+# Functions on numbers
+c = round(8.187, 2)    # Round up to the second decimal point
+print(c)
+
+# Conversions between integers and floats
+c = float(a)
+d = int(b)    # Rounds it down
+print(c)
+print(d)
