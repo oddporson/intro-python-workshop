607141a9018ba51bb22c136c715d7dbfde7ec1fd
Porson <porsonlee@gmail.com>
2020-11-12 12:15:52 -0800

    improve the tax rate using conditional

---
+++ conditionals.py
@@ -0,0 +1,25 @@
+# we often ened to make decision based on certain factors
+# imagine checking if someone is allowed to ride a rollercoaster:
+
+# are they older than 18?
+#   if so, then they can definitely ride it
+
+# In Python
+# print('Can you ride this rollercoaster?')
+# age = int(input('What is your age?: '))
+
+# if age > 18:
+#     print('Definitely!')
+
+# Syntax:
+# if condition:
+#   Do Stuff
+#   Do some more stuff (can have many lines)
+
+# Else statements
+print('Can you ride this rollercoaster?')
+age = int(input('What is your age?: '))
+if age > 18:
+    print('Definitely!')
+else:
+    print('Sorry kid. Come back in a couple of years.')
--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 # print(msg)
 
 
-subtotal = float(input("What is the subtotal? "))
+subtotal = float(input("What is the subtotal?: "))
+tip_percent = int(input('How much do you want to tip?: '))
+
+if tip_percent < 0:
+    print('Error: tip cannot be negative')
+    exit()
+
 tax_rate = 0.14
 
 tax = subtotal * tax_rate
-total = subtotal + tax
+tip = subtotal * tip_percent/100
+total = subtotal + tax + tip
 total = round(total, 2)
 print(total)
